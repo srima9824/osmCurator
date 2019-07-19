4dfb4ed1c3da04073d5f79b13a6c745422caabd5
srima9824 <srima9824@gmail.com>
2019-07-19 11:05:44 +0530

    added test cases

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -13,3 +13,22 @@ def test_hosts_file(host):
     assert f.user == 'root'
     assert f.group == 'root'
 
+def test_configuration_directory_created(host):
+    '''Check if curator configuration file created'''
+    host.file('/etc/elasticsearch-curator').is_directory
+
+def test_configuration_file_created(host):
+    '''Check if curator configuration file created'''
+    host.file('/etc/elasticsearch-curator/curator.yml').exists
+
+def test_delete_index_file_created(host):
+    '''Check if curator configuration file created'''
+    host.file('/etc/elasticsearch-curator/deleteindex.yml').exists
+
+def test_snapshot_file_created(host):
+    '''Check if curator configuration file created'''
+    host.file('/etc/elasticsearch-curator/snapshot.yml').exists
+
+
+
+
